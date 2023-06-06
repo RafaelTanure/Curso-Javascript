448a316fd7002e844f160c2d448d396c495e2082
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-06 08:59:16 -0300

    02-Desafio_area_circunferencia.js

---
+++ 02-Desafio_area_circunferencia.js
@@ -0,0 +1,7 @@
+let raio = 10 ;
+const PI= 3.141592;
+let areaCirc = PI*raio*raio; 
+
+console.log("O valor da área é" + areaCirc + "m2.");
+//Podemos utilizar  o modelo abaixo para executar o desafio tbm . 
+console.log(Math.PI);
