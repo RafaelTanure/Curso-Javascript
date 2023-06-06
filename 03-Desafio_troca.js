31813c30b6db2315a19109925bd24769aaae1062
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-06 10:23:48 -0300

    03-Desafio_troca.js

---
+++ 03-Desafio_troca.js
@@ -0,0 +1,16 @@
+let a = 7;
+let b = 94;
+
+//depois da troca ... a = 94 e b =7 
+
+// solução do desafio 
+
+//vamos criar uma nova váriavel 
+
+let temp = a;
+
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
