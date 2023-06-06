eeb57a19b60c7d4f6a8ebe12a52e867dd8702e90
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-06 08:43:19 -0300

    06-Dados3.js

---
+++ 06-Dados3.js
@@ -0,0 +1,13 @@
+let idade= 31; 
+//Aqui indica se é do tipo number 
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario= 4578.32 // utilização de pontos flutuantes 
+console.log(typeof salario);
+
+let estaChovendo= true // verdadeiro ou false 
+console.log(typeof estaChovendo);
+
+//Aqui indica se é uma sequencia de caracteres, utilizando String
+console.log(typeof "Teste");
