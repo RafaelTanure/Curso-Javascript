a0b4c6159d13515e49efbc1d60eeb2f237839315
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-12 10:05:32 -0300

    20-Fundamentos_usandoLetEmLoop.js
    Utilizando comando for

---
+++ 20-Fundamentos_usandoLetEmLoop.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+//Vai imrprimir de 0 a 9 sem pegar o valor 10 armazenado anteriormente pelo var, vai gerar um erro de indefinido valor " i ".
+console.log('i = ', i); //Escopo de bloco vai gerar um erro. 
