78ef9bbb9f36f3cf15bbdedc3704a5cf2a55d846
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-12 10:01:33 -0300

    19-Fundamentos_usandoVarEmLoop.js
    Comando For

---
+++ 19-Fundamentos_usandoVarEmLoop.js
@@ -0,0 +1,6 @@
+for(var i = 0; i <10; i++){
+    console.log(i);
+}
+console.log('i= ',i);
+
+//Aqui vai imprimir de 1 a 10, mostrando  o valor de i no final, que recebe o valor de 10  mas só quando pedimos para imrpimir o i que aparece o valor de 10. 
