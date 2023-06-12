3138e90a34684976fea663742bee75aa0f792451
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-12 09:52:04 -0300

    18-Fundamentos_usandoLet1.js

---
+++ 18-Fundamentos_usandoLet1.js
@@ -0,0 +1,11 @@
+let numero = 1; 
+
+{
+    let numero =2 ; 
+    console.log('dentro = ', numero);
+}
+
+console.log('fora = ', numero); 
+
+//Váriaveis definidas com a palavra var, tem escopo global e escopo de funcao.Váriaveis definidas com a palavra let tem escopo global, escopo de função e escopo de bloco.
+// Let tem um escopo a mais. Que é o bloco . Então é importante saber sobre isso. 
