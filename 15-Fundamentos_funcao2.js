52b2832bcc72cae394160077f5ad177594721ff4
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-12 09:24:25 -0300

    15-Fundamentos_funcao2.js

--- 15-Fundamentos_funcao2.js
+++ 15-Fundamentos_funcao2.js
@@ -17,3 +17,7 @@ console.log(soma(2, 3));
 //retorno implícito 
 const subtracao = (a, b) => a - b;
 console.log(subtracao(2, 3)); // Aqui será passado o valor -1.
+
+
+const imprimir2 = a => console.log(a);
+imprimir2 ('legal !!!');  // imprime legal. 
