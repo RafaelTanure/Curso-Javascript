89fea91a647b86ae963234c6ae55f3ac66cb9adf
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-13 09:59:59 -0300

    35-Fundamentos_unários.js

---
+++ 35-Fundamentos_unários.js
@@ -0,0 +1,11 @@
+let num1 = 1;
+let num2 = 2;
+
+num1++; // forma pós fixada acrescenta um numero a mais 
+console.log(num1);
+
+--num1; //Forma pré fixada decrementa um número a menos 
+console.log(num1)
+
+console.log(++num1 === num2--); // O resultado dá verdadeiro aqui por que num1 passa a ser maior que num2 
+console.log(num1 === num2); 
