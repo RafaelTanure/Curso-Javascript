51f55e7c7cdc19ceafc685f7506b78b0db4bfe40
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-13 10:04:07 -0300

    36-Fundamentos_ternario.js

---
+++ 36-Fundamentos_ternario.js
@@ -0,0 +1,5 @@
+const resultado = nota  => nota >=7 ? 'Aprovado' : 'Reprovado' ; 
+//Utilização de Operador ternário para que retorne true ou false verificando se a nota é maior ou menor que 7.
+
+console.log(resultado(7.1)); 
+console.log(resultado(6.7)); 
