4a9d8e87fcd80f01c01dc163ef3399481571108a
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-13 08:53:54 -0300

    31-Fundamentos_aritimeticos.js

---
+++ 31-Fundamentos_aritimeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d; // Operador de soma
+const subtracao = d - b; // Operador de subtração 
+const multiplicacao = a * b; // Operador de multiplicação
+const divisao = d / a; // operador de divisão 
+const modulo = a % 2; // Operador que pega o resto da operação 
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
