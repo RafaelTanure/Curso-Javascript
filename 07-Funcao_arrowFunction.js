0867a61b5dba71c0849f73e1b67fbafa649d7763
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-16 09:11:06 -0300

    07-Funcao_arrowFunction.js

---
+++ 07-Funcao_arrowFunction.js
@@ -0,0 +1,19 @@
+let dobro = function (a) {
+    return 2 * a;
+}
+
+dobro = (a) => {
+    return 2 * a;
+}
+
+dobro = a => 2 * a
+console.log(dobro(Marh.PI));
+
+let ola = function () {
+    return 'Olá';
+}
+
+ola = () => 'Olá';
+ola = _ => 'Olá';// possui um paramentro 
+
+console.log(ola());
