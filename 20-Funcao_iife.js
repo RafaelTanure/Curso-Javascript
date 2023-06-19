e013015ebdda3da0adf0dcde6876af891a3e5490
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-19 10:54:53 -0300

    20-Funcao_iife.js

---
+++ 20-Funcao_iife.js
@@ -0,0 +1,7 @@
+// IIFE - > IMMEDIATELY IVOKED FUNCTION EXPRESSION . Função imediatamente invocada
+
+(function() {
+    console.log('Será executado na hora !');
+    console.log('Foge do escopo mais abrangente ! ');
+
+}) ()
