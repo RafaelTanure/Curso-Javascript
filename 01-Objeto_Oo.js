456c4cefbe2bea90b66507c187427859c90bbcf5
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-20 10:05:59 -0300

    01-Objeto_Oo.js

---
+++ 01-Objeto_Oo.js
@@ -0,0 +1,22 @@
+//CÓDIGO NÃO EXECUTÁVEL / CONCEITOS DO OO 
+
+//Procedural 
+processamento (valor1,valor2,valor3)
+
+// OO 
+
+objeto = { 
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+objeto.processamento(); // Foco passou a ser o objeto 
+
+//Princiopios Importantes
+//1.abstração
+//2.encapsulamento 
+//3.Herança (Prototype)
+//4.Polimorfismo multiplas formas 
