50fc186d82721cdd794db87ab9b341191b9da5d6
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2023-06-27 10:46:08 -0300

    15-Array_concat.js

---
+++ 15-Array_concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeskah', 'Cibalena'];
+const filhos = ['Uoxiton', 'Uesclei'];
+const todos = filhas.concat(filhos, 'Fulano');
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], [[6, 7]]));
